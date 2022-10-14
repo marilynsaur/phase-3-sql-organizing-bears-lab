625637e4eca72087eece2166e7ef5f44401662d1
Marilyn Wren <Marilynwren64@yahoo.com>
2022-10-13 20:02:26 -0400

    commit tree 0d11368fad48284a1e54219002d94ede809db353

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id) VALUES (8);
+INSERT INTO bears (name, sex, age) VALUES ('Mr. Chocolate', 'M', 8);
+INSERT INTO bears (name, sex, age) VALUES ('Rowdy', 'M', 9);
+INSERT INTO bears (name, sex, age) VALUES ('Tabitha', 'F', 10);
+INSERT INTO bears (name, sex, age) VALUES ('Sergeant Brown', 'M', 11);
+INSERT INTO bears (name, sex, age) VALUES ('Melissa', 'F', 12);
+INSERT INTO bears (name, sex, age) VALUES ('Grinch', 'M', 13);
+INSERT INTO bears (name, sex, age) VALUES ('Wendy', 'F', 14);
